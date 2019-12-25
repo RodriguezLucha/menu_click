72d35d582d25b059f8a60f5150c7c8da52cd75c3
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-24 21:17:45 -0800

    menu click commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class Cats extends Component {
+  render(){
+    return (
+      <div className="cats">
+        Cats
+      </div>
+    );
+  } 
+}
+class Dogs extends Component {
+  render(){
+    return (
+      <div className="dogs">
+        Dogs
+      </div>
+    );
+  } 
+}
+class Footer extends Component {
+  render(){
+    return (
+      <div className="footer">
+        Footer
+      </div>
+    );
+  } 
+}
+
+class Menu extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      shown: false
+    }
+  }
+
+  toggleMenu() {
+    this.setState({ shown: !this.state.shown });
+  }
+
+  clickedMenuBox(e) {
+    e.stopPropagation();
+  }
+
+  getMenu() {
+    if(!this.state.shown){
+      return;
+    }
+
+    return (
+      <div className="menu_box" onClick={(e)=>this.clickedMenuBox(e)}>
+        <ul>
+          <li><Link to="/home" onClick={() => this.toggleMenu()} >Home</Link></li>
+          <li><Link to="/dogs">Dogs</Link></li>
+          <li><Link to="/cats">Cats</Link></li>
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="menu">
+        <div className="burger" onClick={() => this.toggleMenu()}>
+          <i className="fas fa-bars"></i>
+          {this.getMenu()}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter : 0
+    }
+  }
+
+  appClicked(e) {
+    console.log('target:', e.target);
+    console.log('currentTarget:', e.currentTarget);
+    
+  }
+
+  render() {
+    return (
+      <div className="app" onClick={(e) => this.appClicked(e)}>
+        <Router>
+            <Menu/>
+            <Switch>
+              <Route path="/dogs">
+                <Dogs/>
+              </Route>
+              <Route path="/cats">
+                <Cats/>
+              </Route>
+            </Switch>
+            <Footer/>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
